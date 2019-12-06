bcf36073517aa34066b0661ed308135dc244d815
Michael Diente <michael.diente@englishcentral.com>
2019-12-07 00:47:38 +0800

    Appropriate formating for output -> Cycle count, Remainder(A), Quotient(Q)

--- simulation.asm
+++ simulation.asm
@@ -1,9 +1,9 @@
 %include "io.inc"
 section .data
 VARN db 16 ;number of bits. for this case, n = 16 (n)
-VARM dw 4 ;serves as divisor (M). should be same with VARMN
-VARMN dw 4 ;serves as 2's complement of (M). should be same with VARM
-VARQ dw 5 ;serves as dividend (Q)
+VARM dw 40 ;serves as divisor (M). should be same with VARMN
+VARMN dw 40 ;serves as 2's complement of (M). should be same with VARM
+VARQ dw 500 ;serves as dividend (Q)
 VARA dw 0 ;serves as remainder. init value should be 0 (A)
 section .bss
 RES1 resd 1 ;container for shift left
@@ -22,112 +22,65 @@ CMAIN:
     xor eax, eax
     ret
 START:
-    PRINT_STRING "N: "
+    PRINT_STRING "Cycle: "
     PRINT_UDEC 1, VARN
-    NEWLINE
-    
-    PRINT_STRING "dx: "
-    PRINT_UDEC 2, dx
-    PRINT_STRING " ax: "
-    PRINT_UDEC 2, ax
+    PRINT_STRING ","
     
     mov [RES1], ax
     mov [RES1 + 2], dx
     shl dword [RES1], 1
     
-    NEWLINE
-    PRINT_STRING "Shift left:"
-    PRINT_UDEC 4, RES1
-    NEWLINE
-    
     ; Assign to respective registers
     mov dx, [RES1 + 2]
     mov ax, [RES1]
-    
-    PRINT_STRING "A: "
-    PRINT_UDEC 2, dx
-    NEWLINE
     
     cmp dx, 32767 ;0111111111111111 = max positive number if 16 bits. negative if 1xxxxxxxxxxxxxxx
     jbe A_MINUS_M
     call A_PLUS_M
     ret
 A_MINUS_M:
-    PRINT_STRING "Greater than zero"
-    NEWLINE  
-    
-    PRINT_STRING "M="
-    PRINT_UDEC 2, VARM
     mov bx, [VARMN]
-    PRINT_STRING " -M: "
-    PRINT_UDEC 2, bx
-    NEWLINE 
-    
-    ;Perform A-M
     add dx, bx
-    PRINT_STRING "A-M: "
-    PRINT_UDEC 2, dx
-    NEWLINE
-
+    
     cmp dx, 32767 ;0111111111111111 = max positive number if 16 bits. negative if 1xxxxxxxxxxxxxxx
     jbe ADD_ONE_TO_Q
     call ADD_ZERO_TO_Q
     ret
 A_PLUS_M:
-    PRINT_STRING "Less than zero."
-    NEWLINE
-    
     mov bx, [VARM]
     add dx, bx
-    PRINT_STRING "A+M: "
-    PRINT_UDEC 2, dx
-    NEWLINE
     
     cmp dx, 32767 ;0111111111111111 = max positive number if 16 bits. negative if 1xxxxxxxxxxxxxxx
     jbe ADD_ONE_TO_Q
     call ADD_ZERO_TO_Q
     ret
-ADD_ZERO_TO_Q:
-    PRINT_STRING "Add 0 to Q0"
-    NEWLINE
-    
+ADD_ZERO_TO_Q:    
     add word ax, 0h
-    PRINT_STRING "A: "
-    PRINT_UDEC 2, dx
+    PRINT_STRING " A: "
+    PRINT_HEX 2, dx
     PRINT_STRING " Q: "
-    PRINT_UDEC 2, ax
-    NEWLINE
+    PRINT_HEX 2, ax
     NEWLINE
     
     dec byte [VARN]
-    
     cmp byte [VARN], 1
     jae START
     call COUNT_IS_ZERO
     ret
 ADD_ONE_TO_Q:
-    PRINT_STRING "Add 1 to Q0"
-    NEWLINE
-    
     add word ax, 1h
-    PRINT_STRING "A: "
-    PRINT_UDEC 2, dx
+    PRINT_STRING " A: "
+    PRINT_HEX 2, dx
     PRINT_STRING " Q: "
-    PRINT_UDEC 2, ax
-    NEWLINE
+    PRINT_HEX 2, ax
     NEWLINE
     
     dec byte [VARN] 
-    
     cmp byte [VARN], 1
     jae START
     call COUNT_IS_ZERO
     ret
-COUNT_IS_ZERO:
-    PRINT_STRING "N: "
-    PRINT_UDEC 1, VARN
-    NEWLINE
-    
+COUNT_IS_ZERO:    
     cmp dx, 32767 ;0111111111111111 = max positive number if 16 bits. negative if 1xxxxxxxxxxxxxxx
     jbe END
     
@@ -136,10 +89,13 @@ COUNT_IS_ZERO:
     call END
     ret
 END:
-    PRINT_STRING "A: "
-    PRINT_UDEC 1, dx
+    PRINT_STRING "Cycle: "
+    PRINT_UDEC 1, VARN
+    PRINT_STRING ","
+    PRINT_STRING " A: "
+    PRINT_HEX 2, dx
     PRINT_STRING " Q: "
-    PRINT_UDEC 1, ax
+    PRINT_HEX 2, ax
     NEWLINE
     ret
     
